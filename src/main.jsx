388df937e1f8624e9e31e7aee0c0f5241fb8b13e
Johnny Estrada <johnnye562@yahoo.com>
2023-03-21 22:36:43 -0700

    Import Browser Router and Css baseline to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import "./assets/index.css";
+import { CssBaseline } from "@mui/material";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <CssBaseline enableColorScheme />
+      <App />
+    </React.StrictMode>
+  </Router>
 );
